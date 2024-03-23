acd6134337d41a3ebe217e5c49ab542def35db20
Maged Saif <magedsaif21@gmail.com>
2024-03-23 16:20:31 +0200

    removing unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { useState } from "react";
 import "./index.css";
 import App from "./App";
-
-import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
