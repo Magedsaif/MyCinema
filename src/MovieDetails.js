8bc18ff114893dc78390b25834a7d9c2acf0b799
Maged <magedsaif21@gmail.com>
2024-03-24 14:17:14 +0200

    Update MovieDetails.js

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -63,7 +63,7 @@ export function MovieDetails({
       async function getMovieDetails() {
         setIsloading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
